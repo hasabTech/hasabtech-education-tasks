7c10ab04ab901019ba57cb8b33fcb6d849821dd1
ammad-ahmed-siddiqui <ammad.ahmed.siddiqui@gmail.com>
2025-08-17 02:24:36 +0500

    task2 completed

--- js-basics/tasks/task2.js
+++ js-basics/tasks/task2.js
@@ -1,5 +1,6 @@
 // Task 2: Create a constant PI with value 3.14 and export it.
-
+const PI = 3.14;
+module.exports = { PI };
 // TODO: Create a constant called 'PI' with value 3.14
 
 // TODO: Export the PI constant
