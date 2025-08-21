72316c9f801cb1054e15bfd60a2add5b261e1a11
nabhaht <nabahatshah@gmail.com>
2025-08-21 16:45:43 -0700

    TASK-JS-BASICS-NABHAH: complete all tasks

--- js-basics/tasks/task5.js
+++ js-basics/tasks/task5.js
@@ -8,8 +8,21 @@
 // 60-69: "D"
 // Below 60: "F"
 
-// function getGrade(score) {
-//   // Your code here using if-else statements
-// }
+function getGrade(score) {
+    if (score >= 90) {
+      return "A";
+    } else if (score >= 80) {
+      return "B";
+    } else if (score >= 70) {
+      return "C";
+    } else if (score >= 60) {
+      return "D";
+    } else {
+      return "F";
+    }
+  }
+  
+ 
 
 // TODO: Export the getGrade function
+module.exports = {getGrade}
