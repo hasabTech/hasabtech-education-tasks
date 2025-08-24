8d8ff0673803ba035acdf5cd917138fd1c2fdef8
ammad-ahmed-siddiqui <ammad.ahmed.siddiqui@gmail.com>
2025-08-24 10:48:05 +0500

    My all task

--- js-basics/tasks/task7.js
+++ js-basics/tasks/task7.js
@@ -3,9 +3,16 @@
 // TODO: Create a function called 'sumArray' that takes one parameter (arr)
 // TODO: Use a loop to iterate through the array and sum all numbers
 // TODO: Return the total sum
-
+function sumArray(arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
+}       
 // function sumArray(arr) {
 //   // Your code here using a for loop or for...of loop
 // }
 
 // TODO: Export the sumArray function
+module.exports = { sumArray };
