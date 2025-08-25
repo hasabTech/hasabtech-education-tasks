d2949f12f9102020b877fe4b47050b8dae997c1b
Faizan <faizan228918@gmail.com>
2025-08-25 19:48:27 +0500

    Completed task2.js
    All test cases pass in local server.

--- js-basics/tasks/task2.js
+++ js-basics/tasks/task2.js
@@ -1,5 +1,4 @@
 // Task 2: Create a constant PI with value 3.14 and export it.
 
-// TODO: Create a constant called 'PI' with value 3.14
-
-// TODO: Export the PI constant
+const PI = 3.14;
+module.exports = { PI };
