d700a569b044b47ba5fc551b405afa514faeaf74
Faizan <faizan228918@gmail.com>
2025-08-25 20:03:26 +0500

    Completed task5.js
    All tests pass locally.

--- js-basics/tasks/task5.js
+++ js-basics/tasks/task5.js
@@ -1,15 +1,16 @@
 // Task 5: Write a function getGrade(score) that returns "A", "B", "C", "D", "F" based on score.
 
-// TODO: Create a function called 'getGrade' that takes one parameter (score)
-// TODO: Return grades based on the following scale:
-// 90-100: "A"
-// 80-89: "B" 
-// 70-79: "C"
-// 60-69: "D"
-// Below 60: "F"
-
-// function getGrade(score) {
-//   // Your code here using if-else statements
-// }
-
-// TODO: Export the getGrade function
+function getGrade(score) {
+  if (score >= 90 && score <= 100) {
+    return "A";
+  } else if (score >= 80 && score < 90) {
+    return "B";
+  } else if (score >= 70 && score < 80) {
+    return "C";
+  } else if (score >= 60 && score < 70) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
+module.exports = { getGrade };
